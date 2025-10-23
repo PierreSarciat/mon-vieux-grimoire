381aea9c6906ef4201adc2fa47a6ceeab392d4ca
PierreSarciat <phsarciat@mailo.com>
2025-10-23 17:59:30 +0200

    dossier route et schema utilisateur

--- serveur.js
+++ serveur.js
@@ -1,46 +1,31 @@
 /***********import des modules nécessaires******/
 
-require('dotenv').config(); // charge les variables depuis .env
- const express = require('express');
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const stuffRoutes = require('./backend/routes/stuff'); 
 
-/********import du modèle ***********/
-
-
-const Thing = require('./backend/models/thing');
-
-
-/*********connexion MongoDB********** */
-
+/*********connexion MongoDB**********/
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log("Connexion MongoDB réussie !"))
   .catch(err => console.error("Erreur de connexion :", err));
 
-
-
-
-/**********initialisation de l api******* */
-
 const app = express();
 
-/*app.use(cors());const cors = require('cors');*/
-
 app.use(cors({
-    origin: 'http://localhost:3000', // autorise le frontend React
-    methods: ['GET','POST','PUT','DELETE'], // méthodes autorisées
-    credentials: true
+  origin: 'http://localhost:3000',
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ✅ ici on "monte" les routes
+app.use('/api', stuffRoutes);
 
-
-/**********démarrage serveur********* */
-
- app.listen(process.env.SERVER_PORT, () => {
-      console.log(` Server is running on port ${process.env.SERVER_PORT}`);
-    });
-
-
-  
+/**********démarrage serveur**********/
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
